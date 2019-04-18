aab11ad0ee68cb8ab72d98b256110d73b97846ea
Jordan <dominguez.jordan1@gmail.com>
2019-04-18 08:39:37 +0200

    Fixing bug
    Correction d'un bug empechant la l'utilisation de l'application sur d'autre pc

--- public/js/ratingBar.js
+++ public/js/ratingBar.js
@@ -1,7 +1,9 @@
 $(function () {
     $('.rating').barrating({
         theme: 'bars-1to10',
+        allowEmpty: true,
         emptyValue: 0,
+        
         onSelect: function (value, text, event) {
             var apiid = this.$elem.attr('id')
             var title = this.$elem.attr('data-title')
